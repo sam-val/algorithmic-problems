51bfbe754561b60bb36eac7205d52eef30fdab1e
Sam Val <samvalentine2811@gmail.com>
2021-01-12 20:19:13 +0700

    update lowest common ancestor

--- lowest_common_ancestors.py
+++ lowest_common_ancestors.py
@@ -1,45 +1,47 @@
-class Node:
-    def __init__(self,left=None, right=None):
-        self.value = None
-        self.left = left
-        self.right = right
+# I'm using binarytree module from PyPI -- pip install binarytree
+# But the basic algorithm goes like this:
 
-    def add(self,node):
-        a = self.left is None
-        b = self.right is None
-        if a and b:
-            import random
-            if random.random() < 0.5:
-                self.left = node
-            else:
-                self.right = node
-        elif a:
-            self.left = node
-        elif b:
-            self.right = node
-        else:
-            import random
-            if random.random() < 0.5:
-                self.right.add(node)
-            else:
-                self.left.add(node)
+# ...
+# 1. Traverse tree and find the first element, remember this path (as list)
+# 2. Traverse tree and find the second element, remember this path
+# 3. The lowest common ancestor is the cross-point between the two paths/lists (by comparing the reversed paths of the two).
+# ...
 
+from binarytree import tree, get_parent
 
+def find_path(tree, node):
+    # assume node exists in tree
+    path = []
+    current_node = node
+    while current_node != None:
+        temp = current_node
+        path.insert(0, current_node)
+        current_node = get_parent(tree, current_node)
+        if current_node == None:
+            if temp != tree[0]:
+                path.pop(0)
+            break
 
-class Tree:
-    def __init__(self,root):
-        self.root = root
-
-    def add(self,node):
-        self.root.add(node)
+    return path
 
 
+def solve(tree, first, second):
+    rs = None
+    
+    # assume #1 and #2 nodes exist in tree
 
+    path_first = find_path(tree, first)
+    print(path_first)
+    path_second = find_path(tree, second)
+    print(path_second)
 
-def solve():
-    pass
+    if (len(path_first) == 0 or len(path_second) == 0):
+        return rs
 
+    for i in range(len(path_first)-1,0,-1):
+        for j in range(len(path_second)-1,0,-1):
+            if path_first[i] == path_second[j]:
+                rs = path_first[i];
 
-root = Node()
-tree = Tree(root)
-tree.add(Node())
+    return rs
+
